33ad738e991dfc2fa1fd1fbbca1515593a026850
Sebastian009w <ia3266505@gmail.com>
2023-02-21 21:15:00 -0500

    documentacion generada

--- src/index.py
+++ src/index.py
@@ -1,11 +1,32 @@
+"""
+ #!-importancion del framewwork Flask
+"""
 from flask import Flask, render_template
 
+"""
+ !# Inicializo la aplicacion de Flask uso: __name__ , para decirle que es una funcion princiapl
+"""
 app = Flask(__name__)
 
+
+"""
+!# ya inicilizada la aplicacion creo la primera ruta.
+"""
 @app.route("/")
 def home():
+    """
+    #! - Atraves de la funcion "render_template" envio un HTML al cliente.
+    """
     return render_template("index.html")
 
 
+"""
+#! Valido si la variable que estoy recibiendo es la principal.
+"""
 if __name__ == '__main__':
+    """
+        #! Arranco el servidor
+    """
     app.run(debug=True)
+
+print("Genial Has renderizado Un HTML")

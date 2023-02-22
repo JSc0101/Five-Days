527410bed3fe9c3bbd028deec1f7a126abe16215
Sebastian009w <ia3266505@gmail.com>
2023-02-21 21:06:23 -0500

    ejemplo finalizado

--- src/index.py
+++ src/index.py
@@ -4,19 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("app.html")
+    return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/service")
-def service():
-    return render_template("services.html")
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
 
 if __name__ == '__main__':
     app.run(debug=True)

105d494ce69b806bc8695d8617e1d7253be9ad0d
Sebastian009w <ia3266505@gmail.com>
2023-02-21 20:33:26 -0500

    renderizando ultimos archivos

--- index.py
+++ index.py
@@ -10,5 +10,13 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/service")
+def service():
+    return render_template("services.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
